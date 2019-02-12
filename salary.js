fa35cc138f6ab2989590592c12ee5d759e523ad8
shaxqq <39559342+shaxqq@users.noreply.github.com>
2019-02-12 20:36:12 +0200

    Update salary.js

--- salary.js
+++ salary.js
@@ -1,13 +1,22 @@
 sumInputs = function() {
-    var inputs = document.getElementsByTagName('input'),
-        result = document.getElementById('total'),
+    var inputs = document.getElementsByTagName('input');
+        result = document.getElementById('total');
+
+    var tarif = document.getElementById('tariff').value;
+    tarif = parseInt(tarif);
+    var disc = document.getElementById('discount').value;
+    disc = parseInt(disc);
+    var tarifsum = tarif - ((tarif * disc) / 100);
+    console.log(tarifsum);
+
         sum = 0;            
     
     for(var i=0; i<inputs.length; i++) {
         var ip = inputs[i];
     
-        if (ip.id && ip.id.indexOf("total")) {
+        if (ip.id && ip.id.indexOf("total")  ) {
             sum += parseInt(ip.value) || 0;
+            
         }
     
     }
