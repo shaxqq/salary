d51d2a2b0be37b0e2f4286dabcc4aa9cd081b8b5
shaxqq <39559342+shaxqq@users.noreply.github.com>
2019-02-15 10:23:54 +0200

    Update salary.js

--- salary.js
+++ salary.js
@@ -1,27 +1,29 @@
 sumInputs = function() {
-    var inputs = document.getElementsByTagName('.salary__inner input');
-        result = document.getElementById('total');
-
     var tarif = document.getElementById('tariff').value;
     tarif = parseInt(tarif);
     var disc = document.getElementById('discount').value;
     disc = parseInt(disc);
-    var tarifsum = tarif - ((tarif * disc) / 100);
-    // console.log(tarifsum);
+    var tarifsum = tarif - ((tarif * disc) / 100) || 0;   
+    console.log(tarifsum);
 
-        sum = 0;            
+
+    var inputs = document.getElementsByTagName('input');
+    result = document.getElementById('total');
     
+        sum = tarifsum;
+        console.log(sum);
     for(var i=0; i<inputs.length; i++) {
         var ip = inputs[i];
-    
-        if (ip.id && ip.id.indexOf("total")  ) {
+        console.log(ip);
+
+        if (ip.id && ip.id.indexOf("total") < 0 ) {
             sum += parseInt(ip.value) || 0;
-            
         }
-    
+
     }
-    console.log(result.value =  parseInt(tarifsum+sum));
-    console.log(result.value =  sum);
-    console.log(result.value =  tarifsum);
-    result.value = parseInt(tarifsum+sum);
+
+    console.log(sum);
+    
+    
+    result.value = sum;
 }
