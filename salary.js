6d1e1db8bda0023725dd48b2f4203f59a7c16360
shaxqq <39559342+shaxqq@users.noreply.github.com>
2019-02-16 20:14:05 +0200

    salary

--- salary.js
+++ salary.js
@@ -66,31 +66,35 @@ function sumDay() {
 	var date1 = new Date(firstDate());
 	var date2 = new Date(secondDate());
 	var timeDiff = Math.abs(date2 - date1);
-	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24) + 1);
+	var diffDays = timeDiff / (1000 * 3600 * 24) + 1;
 	var allSum = sumAll();
 	var fromYear = date1.getFullYear();
 	var fromMonth = date1.getMonth();
 	var firstMonthDaysTotal = new Date(fromYear, fromMonth + 1, 0).getDate();
 	var perDay = allSum / firstMonthDaysTotal;
 	var result = diffDays * perDay || 0;
-	document.getElementById("out3").innerHTML = result.toFixed(2);	
-	console.log("разниці мс", timeDiff);
-	console.log(date2.getFullYear());
-	console.log("номер месяца", date1.getMonth());
-	console.log("дни в сумме ", diffDays);
-	console.log(fromYear);
-	console.log(fromMonth);
-	console.log("Дней в вибранном месяце", firstMonthDaysTotal);
-	console.log(" Один день ", perDay);
-	console.log("За все дни", result);
-
-	
+	document.getElementById("out3").innerHTML = parseFloat(result.toFixed(2))+parseInt(minus());	
+	// console.log("разниці мс", timeDiff);
+	// console.log(date2.getFullYear());
+	// console.log("номер месяца", date1.getMonth());
+	// console.log("дни в сумме ", diffDays);
+	// console.log(fromYear);
+	// console.log(fromMonth);
+	// console.log("Дней в вибранном месяце", firstMonthDaysTotal);
+	// console.log(" Один день ", perDay);
+	// console.log("За все дни", result);	
 }
 
 function minus() {
 	var minus = document.getElementById('minus').value;
-	var m = parseInt(minus);
-	return m
+	if(minus == null || minus == '' || minus == undefined){
+		minus = 0;
+	} else {
+		minus =  parseInt(minus);
+	}
+	return minus
+
+	// return m
 }
 
 function outDate() {
